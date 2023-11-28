551c95d5e92d19c88210dd785437f1f408edfd95
Dima Birichev <dbirichev@gmail.com>
2023-11-28 20:41:32 +0300

    primitive logic for adding a banner

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,23 @@
-// const banner = document.querySelector('.banner__container')
-// const label = document.querySelector('.banner__slider')
+const banner = document.querySelector('.banner__container')
+const label = document.querySelector('.banner__slider')
 
-// function createBanner (res) {
-//     const bannerTemplate = document.querySelector('#banner-template').content
-//     const bannerElement = bannerTemplate.querySelector('.slide').cloneNode(true)
-//     const bannerName = bannerElement.querySelector('.slide__name')
+function createBanner (res) {
+    const bannerTemplate = document.querySelector('#banner-template').content
+    const bannerElement = bannerTemplate.querySelector('.slide').cloneNode(true)
+    const bannerName = bannerElement.querySelector('.slide__name')
     
 
-//     bannerName.textContent = res.nameOriginal
-//     bannerElement.style.setProperty('--poster-3', `url(${res.coverUrl})`)
+    bannerName.textContent = res.nameOriginal
+    bannerElement.style.setProperty('--poster-3', `url(${res.coverUrl})`)
 
-//     return bannerElement
-// }
+    return bannerElement
+}
 
-// function createLabel(res) {
-//     const labelTemplate = document.querySelector('#label-template')
-//     const labelElement = labelTemplate.querySelector('.banner__slider-label')
-//     return labelElement
-// }
+function createLabel(res) {
+    const labelTemplate = document.querySelector('#label-template')
+    const labelElement = labelTemplate.querySelector('.banner__slider-label')
+    return labelElement
+}
 
 // function uploadBanner (data) {
 //     banner.append(createBanner(data))
